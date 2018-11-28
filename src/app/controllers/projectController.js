ff75faf340b50a0ea82406dcb2793b89af468b8c
Afonso Tessari Ferreira <afonsotessari@gmail.com>
2018-11-28 10:27:24 -0200

    Update projectController.js

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -5,13 +5,36 @@ const Poster = require('../models/poster');
 const router = express.Router();
 
 router.use(authMiddleware);
-
+/*
 router.get('/', async (req, res) => {
     try{
+      
+        
         const {page = 1 } = req.query;
         const posters = await Poster.paginate({}, { page, limit:10});
 
+
         return res.send(posters);
+    } catch(err){
+        return res.status(400).send({ error: 'Error loading new poster'});
+    }
+});*/
+
+router.get('/', async (req, res) => {
+    try{
+      
+        
+        const { page, perPage } = req.query;
+        const options = {
+          page: parseInt(page, 30) || 1,
+          limit: parseInt(perPage, 30) || 30,
+          populate: 'user',
+        };
+        const posters = await Poster.paginate({}, options);
+        return res.json(posters);
+
+
+       // return res.send(posters);
     } catch(err){
         return res.status(400).send({ error: 'Error loading new poster'});
     }
@@ -24,18 +47,20 @@ router.get('/:posterId', async (req, res) => {
 router.post('/', async (req, res) => {
     
     try{
-        const poster = await Poster.create({...req.body, user: req.name});
+        const poster = await Poster.create({...req.body, user: req.userId});
 
-        return res.send({ poster });
+        const posterPopulated = await Poster.findOne({_id: poster._id}).populate('user');
+
+        return res.send({poster: posterPopulated});
     } catch (err) {
         return res.status(400).send({ error: 'Error creating new poster'});
     }
 });
-/*
+
 router.put('/:posterId', async (req, res) => {
     
 });
-*/
+
 router.delete('/:posterId', async (req, res) => {
     try{
         await Poster.findByIdAndRemove(req.params.posterId);
